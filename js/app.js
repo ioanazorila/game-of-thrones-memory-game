dfa74fcca97b56239fe28b8791e1a38caf872f73
Ioana Zorila <ioanazorila@yahoo.com>
2018-03-29 00:24:09 +0300

    Add win message

--- js/app.js
+++ js/app.js
@@ -1,16 +1,8 @@
-/*
- * Create a list that holds all of your cards
- */
+///////////////////////////////////// Card Values /////////////////////////////////////
+
 const cardValues = ["fa-anchor", "fa-bicycle", "fa-bolt", "fa-bomb", "fa-cube", "fa-diamond", "fa-leaf", "fa-paper-plane-o"];
 const allCardValues = cardValues.concat(cardValues);
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -28,6 +20,9 @@ function shuffle(array) {
 }
 
 
+
+///////////////////////////////////// New Game /////////////////////////////////////
+
 let openCards = [];                     // list of visible cards
 let moves = 0;                          // number of moves
 let startTime;                          // start time for game
@@ -40,23 +35,30 @@ function newGame() {
     const cards = document.querySelectorAll('li.card');
     cards.forEach(function(card, i) {
         card.removeChild(card.firstElementChild);
-        card.insertAdjacentHTML('afterbegin',`<i class="fa ${allCardValues[i]}"></i>`);
+        card.insertAdjacentHTML('afterbegin', `<i class="fa ${allCardValues[i]}"></i>`);
     });
 
     startTime = performance.now();      // update start time
 }
 
 
-// start a new game when the page is reloaded
+// Start a new game when the page is reloaded
 newGame();
 
 
+
+//////////////////////////////////// Restart Game ///////////////////////////////////
+
 function restartGame() {
+    // show deck, hide win message
+    document.querySelector('.deck').classList.remove('hide');
+    document.querySelector('.win-message').classList.add('hide');
+
     openCards = [];                     // reset array that lists visible cards
-    moves = -1;                          // reset the number of moves
+    moves = -1;                         // reset the number of moves
     incrementMoves();
 
-    //hide visible cards
+    // hide visible cards
     const cards = document.querySelectorAll('li.card');
     for (card of cards) {
         card.classList.remove('open');
@@ -75,29 +77,20 @@ function restartGame() {
 }
 
 
-// restart game when the restart button is clicked
+// Restart game when the restart button is clicked
 document.querySelector('div.restart').addEventListener('click', function () {
     restartGame();
 });
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
+/////////////////////////////////// Play the Game ///////////////////////////////////
 
 const deck = document.querySelector('ul.deck');         // element that includes all cards
 let clickAllowed = true;                                // determines if the user is allowed to reveal another card at a given time
 
 
-// event listener for click on a card
+// Event listener for click on a card
 deck.addEventListener('click', function (evt) {
     const currentCard = evt.target;                     // card that was clicked
 
@@ -108,15 +101,16 @@ deck.addEventListener('click', function (evt) {
     if (currentCard.classList.contains('open') || currentCard.tagName != "LI" || !clickAllowed)
         return;
 
-    showCard(currentCard);
-    addToOpenCards(currentCard);
+    showCard(currentCard);                              // display the card's symbol
+    addToOpenCards(currentCard);                        // add to list of visible cards
 
     const noOfOpenCards = openCards.length;
 
-    // when trying to match two cards:
+    // when the user is trying to find matching cards:
     // 1st card that is clicked -> odd number of visible cards (pairs of already visible matching cards + the most recently clicked card)
     // 2nd card that is clicked -> even number of visible cards
     // only if the number of visible cards is even, test if the most recently clicked card matches the previously clicked card
+
     if (!(noOfOpenCards%2)) {
 
         const prevCard = openCards[noOfOpenCards-2];    // previously clicked card
@@ -125,53 +119,52 @@ deck.addEventListener('click', function (evt) {
         if (moves === 10 || moves === 16)
             removeStar();
 
-        // cards match if their "fa-..." classes match
+        // cards match if their "fa-..." classes match (2nd class of each card)
         if (currentCard.firstElementChild.classList.item(1) === prevCard.firstElementChild.classList.item(1)) {
             lockCards(currentCard, prevCard);
             
-            // at this point, if the 2 most recently clicked cards match, and all 16 cards are visible, the user has won
+            // at this point, if the 2 most recently clicked cards matched, and all 16 cards are visible, the user has won
             if (noOfOpenCards === 16) {
-                const endTime = performance.now();
-                console.log("win " + (endTime - startTime)/1000);
+                winMessage();
             }
+
         } else {
             clickAllowed = false;                       // do not allow user to reveal other cards until the non-matching cards are hidden
-            setTimeout(function(){
+            setTimeout(function(){                      // hide non-matching cards after 0.5 seconds
                 hideCards(currentCard, prevCard);
             }, 500);
         }
     }
-
 });
 
 
-// display the card's symbol
+// Display the card's symbol
 function showCard(currentCard) {
     currentCard.classList.add('open');
     currentCard.classList.add('show');
 }
 
 
-// add to list of visible cards
+// Add to list of visible cards
 function addToOpenCards(currentCard) {
     openCards.push(currentCard);
 }
 
 
-// increment number of moves
+// Increment number of moves
 function incrementMoves() {
     document.querySelector('.moves').innerHTML = ++moves;
 }
 
 
-// lock the matching cards in open position
+// Lock the matching cards in open position
 function lockCards(currentCard, prevCard) {
     currentCard.classList.add('match');
     prevCard.classList.add('match');
 }
 
 
-// if the most recently clicked card does not match the previously clicked card, hide the two cards
+// If the most recently clicked card does not match the previously clicked card, hide the two cards
 function hideCards(currentCard, prevCard) {
     currentCard.classList.remove('open');
     currentCard.classList.remove('show');
@@ -182,7 +175,27 @@ function hideCards(currentCard, prevCard) {
 }
 
 
-// remove star
+// Remove star
 function removeStar() {
     document.querySelector('.fa-star').classList.remove('fa-star');
+}
+
+
+
+/////////////////////////////////// Win the game ///////////////////////////////////
+
+// Display win message
+function winMessage() {
+    const endTime = performance.now();
+    const playTime = ((endTime - startTime)/1000).toFixed(2);
+    const stars = document.querySelectorAll('.fa-star').length;
+
+    // update content of win message
+    document.querySelector('.play-time').innerHTML = playTime;
+    document.querySelector('.no-of-moves').innerHTML = moves;
+    document.querySelector('.no-of-stars').innerHTML = (stars === 1) ? `${stars} star` : `${stars} stars`;
+
+    // hide deck and display win message
+    document.querySelector('.deck').classList.add('hide');
+    document.querySelector('.win-message').classList.remove('hide');
 }
